cb477d2524eeb350fd03ff1c17f34554a02f8708
maxim2013pro-debug <maxim2013pro@icloud.com>
2026-09-01 18:06:59 +0300

    Create GameRendererMixin.java

---
+++ src/main/java/net/apexvisuals/GameRendererMixin.java
@@ -0,0 +1,19 @@
+package net.apexvisuals;
+
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+
+    @Inject(method = "tiltViewWhenHurt", at = @At("HEAD"), cancellable = true)
+    private void onHurtCam(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
+        if (ApexVisuals.ApexConfig.disableHurtCam) {
+            ci.cancel();
+        }
+    }
+}
